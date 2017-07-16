d27162450936c7666a0ceba49cbb16cc7ab58487
Luc Bouchard <lucf@bouchard.com>
2017-07-16 14:14:56 -0700

    Update package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  siteMetadata: {
-    siteName: `Using Typescript Example`,
-  },
   plugins: [
-    // Add typescript stack into webpack
-    `gatsby-plugin-typescript`,
+    'gatsby-plugin-typescript',
   ],
+  siteMetadata: {
+    siteName: 'Luc Bouchard Portfolio',
+  },
 }
